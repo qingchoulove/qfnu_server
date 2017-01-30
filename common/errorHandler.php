06de0307d9c90501c4374e2eea6eb25752029ea7
qingchoulove <qingchoulove@hotmail.com>
2017-01-30 16:18:09 +0800

    errorHandler

--- common/errorHandler.php
+++ common/errorHandler.php
@@ -13,16 +13,19 @@ class errorHandler {
     }
 
     public function __invoke($request, $response, $exception) {
+        $logger = static::$app->get('logger');
         $body = [
             'message' => $exception->getMessage()
         ];
+        $detail = [
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'trace' => explode("\n", $exception->getTraceAsString())
+        ];
         if (static::$displayErrorDetails) {
-            $body = array_merge($body, [
-                'file' => $exception->getFile(),
-                'line' => $exception->getLine(),
-                'trace' => explode("\n", $exception->getTraceAsString())
-            ]);
+            $body = array_merge($body, $detail);
         }
+        $logger->error(json_encode(array_merge($body, $detail)));
         return $response
             ->withStatus(500)
             ->withHeader('Content-Type', 'application/json')
