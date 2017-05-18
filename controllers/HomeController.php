890426d6deb90c8e87894e409e425e516d3c79f8
qingchoulove <qingchoulove@hotmail.com>
2017-05-18 20:50:25 +0800

    完善图书借阅查询

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,19 +1,15 @@
 <?php
 namespace controllers;
 
-use common\Util;
-
 class HomeController extends BaseController
 {
 
     public function index($request, $response)
     {
-        // $result = [
-        //     'status' => true,
-        //     'message' => 'hello world'
-        // ];
-        // return $response->withJson($result);
-        $result = $this->libService->getBorrowHistroy('2012416747');
-        Util::Dump($result);
+        $result = [
+            'status' => true,
+            'message' => 'hello world'
+        ];
+        return $response->withJson($result);
     }
 }
