1ece92df4270966c8d0d16760f46a6bb67cbce50
SoftpaseFar <kang_hui1314@126.com>
2017-06-05 11:14:48 +0800

    过滤器优化&异常框架

---
+++ Exception/LoginException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SoftpaseFar
+ * Date: 2017/6/5
+ * Time: 11:09
+ */
+
+namespace Exception;
+
+
+class LoginException extends BaseException
+{
+
+}
--- validators/LoginValidator.php
+++ validators/LoginValidator.php
@@ -12,37 +12,25 @@ namespace validators;
 class LoginValidator extends BaseValidator
 {
     protected $rule = [
-        'user_id' => 'require|isPositiveInteger|length:10',
+        'user_id' => 'require|PositiveInteger|length:10',
         'password' => 'require|length:6',
-        'captcha' => 'withoutValidator'
     ];
 
     protected $message = [
 
         'user_id.require' => '账号必须填写',
-        'user_id.isPositiveInteger' => '账号格式有误',
+        'user_id.PositiveInteger' => '账号格式有误',
         'user_id.length' => '账号长度不合法',
         'password.require' => '密码必须填写',
         'password.length' => '密码长度不合法',
     ];
 
-    /**
-     * 必须是正整数(自带number)
-     * @param $value
-     * @return bool
-     */
-    protected function isPositiveInteger($value)
-    {
-        if (is_numeric($value) && is_int($value + 0) && ($value + 0) > 0) {
-            return true;
-        }
-        return false;
-    }
 
+    protected $filter = [
+        'user_id',
+        'password',
+        'captcha',
+    ];
 
-    protected function withoutValidator()
-    {
-        return true;
-    }
 
 }
