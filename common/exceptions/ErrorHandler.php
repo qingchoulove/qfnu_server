56d6bae8f39c71a4c22d79752059d2de9a7bc257
qingchoulove <qingchoulove@hotmail.com>
2017-06-15 21:41:25 +0800

    完善异常

--- common/exceptions/ErrorHandler.php
+++ common/exceptions/ErrorHandler.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 
 class ErrorHandler extends Component
 {
-    public function __invoke(Request $request, Response $response, $exception)
+    public function __invoke(Request $request, Response $response, Exception $exception)
     {
         $logger = $this->get('logger');
         $displayErrorDetails = $this->get('settings')['displayErrorDetails'];
@@ -22,10 +22,6 @@ class ErrorHandler extends Component
             $httpCode = $exception->getHttpCode();
         } elseif ($exception instanceof BaseException) {
             $httpCode = $exception->getHttpCode();
-        } else {
-            if (!$displayErrorDetails) {
-                $body['message'] = '服务器内部错误';
-            }
         }
 
         $detail = [
