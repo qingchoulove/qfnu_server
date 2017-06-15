04ebbf6878c4169d4ffa4a2fa4b491d3f4766cbb
qingchoulove <qingchoulove@hotmail.com>
2017-06-15 22:32:26 +0800

    调整validator

--- common/exceptions/FieldNotValidException.php
+++ common/exceptions/FieldNotValidException.php
@@ -8,11 +8,7 @@ class FieldNotValidException extends BaseException
     protected $message = "字段校验失败";
     protected $httpCode = 500;
     protected $errorInfo = [];
-<<<<<<< HEAD
-    protected $code = 1002;
-=======
     protected $code = 1000;
->>>>>>> 完善异常
 
     public function __construct(string $message = "", array $errorInfo, int $code = 0, Throwable $previous = null)
     {
