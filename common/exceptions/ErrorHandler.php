0e9a7eaed5c72241b3e23a8f350fa37e48f806d3
qingchoulove <qingchoulove@hotmail.com>
2017-06-15 22:32:26 +0800

    调整validator

--- common/exceptions/ErrorHandler.php
+++ common/exceptions/ErrorHandler.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 
 class ErrorHandler extends Component
 {
-    public function __invoke(Request $request, Response $response, Exception $exception)
+    public function __invoke(Request $request, Response $response, $exception)
     {
         $logger = $this->get('logger');
         $displayErrorDetails = $this->get('settings')['displayErrorDetails'];
@@ -22,6 +22,10 @@ class ErrorHandler extends Component
             $httpCode = $exception->getHttpCode();
         } elseif ($exception instanceof BaseException) {
             $httpCode = $exception->getHttpCode();
+        } else {
+            if (!$displayErrorDetails) {
+                $body['message'] = '服务器内部错误';
+            }
         }
         $detail = [
             'file' => $exception->getFile(),
