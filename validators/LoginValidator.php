60670e402a3c389ae35208ce960343cb4302a0e6
qingchoulove <qingchoulove@hotmail.com>
2017-06-27 10:22:50 +0800

    fix

--- validators/LoginValidator.php
+++ validators/LoginValidator.php
@@ -11,16 +11,12 @@ namespace validators;
 class LoginValidator extends BaseValidator
 {
     protected $rule = [
-        'user_id' => 'require|number|gt:0|length:10',
+        'user_id' => 'require',
         'password' => 'require',
     ];
 
     protected $message = [
-
         'user_id.require' => '账号必须填写',
-        'user_id.number' => '账号格式有误',
-        'user_id.gt' => '账号格式有误',
-        'user_id.length' => '账号长度不合法',
         'password.require' => '密码必须填写',
     ];
 
