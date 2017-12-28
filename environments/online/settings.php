f23101d5d51cca8012e54f4cedaa685e591fd7c2
qingchoulove <qingchoulove@hotmail.com>
2017-12-28 17:45:04 +0800

    增加线上配置

---
+++ environments/online/settings.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => false, // set to false in production
+        'addContentLengthHeader' => true, // Allow the web server to send the content-length header
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'app',
+            'path' => __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::INFO,
+        ],
+        // Wechat settings
+        'wechat' => [
+            'debug'  => true,
+            'app_id' => 'wx4d7bb7798f32eead',//wx84a9581b9181bc8e//wx4d7bb7798f32eead
+            'secret' => 'bd0e8e50a77117ff28b25e0351e6cb5f',//cbc791a2e11e1fd6953a2a5e813f8e86//bd0e8e50a77117ff28b25e0351e6cb5f
+            'token'  => 'easywechat',
+            'log' => [
+              'level' => 'debug',
+              'file'  => __DIR__. '/../logs/easywechat.log',
+            ]
+        ],
+        // Db
+        'db' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'database' => 'zsqy',
+            'username' => 'root',
+            'password' => 'XuXd73oLu9t794me',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+        'redis' => [
+            'scheme'   => 'tcp',
+            'host'     => '127.0.0.1',
+            'port'     => 6379,
+            'database' => 0
+        ]
+    ],
+];
